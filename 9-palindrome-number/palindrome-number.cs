6c20498e6a9363fb622d1cbee31b1a16cee47f36
MohamedEltoum <157864944+MohamedEltoum@users.noreply.github.com>
2025-08-25 17:53:26 +0200

    Time: 1 ms (99.96%) | Memory: 34.6 MB (76.25%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+        
+        int reverse = 0;
+        int original = x;
+
+        while(original > reverse){
+            reverse = reverse * 10 + (original % 10);
+            original /= 10;
+        }
+
+        return reverse == original || original == reverse / 10;
+    }
+}
